5c1b192ff43595b730d7aa5a72cdfcbfe286e1f6
Marcel Felber <marcel.felber.mail@gmail.com>
2025-09-12 12:58:23 +0200

    Updating Security Config to enable access to the Swagger UI, updating imports for angular

--- Game-Hub/game-Hub-be/game-Hub/src/main/java/gamehub/game_Hub/Security/SecurityConfig.java
+++ Game-Hub/game-Hub-be/game-Hub/src/main/java/gamehub/game_Hub/Security/SecurityConfig.java
@@ -29,11 +29,12 @@ public class SecurityConfig {
     http.cors(withDefaults()).csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(req ->
               req.requestMatchers(
-                "/auth/**",
+                  "/auth/**",
+                  "/v2/api-docs",
                   "/v3/api-docs",
-                "/v3/api-docs/**",
-                  "swagger-resources",
-                  "swagger-resources/**",
+                  "/v3/api-docs/**",
+                  "/swagger-resources",
+                  "/swagger-resources/**",
                   "/configuration/ui",
                   "/configuration/security",
                   "/swagger-ui/**",
