89e7816a843f03d6b4634213b3b4ab60fea1ac5a
Marcel Felber <marcel.felber.mail@gmail.com>
2025-10-17 22:33:57 +0200

    Updating CSS styles

--- Game-Hub/game-Hub-fe/src/app/modules/gamehub/gamehub-routing.module.ts
+++ Game-Hub/game-Hub-fe/src/app/modules/gamehub/gamehub-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from './pages/main/main.component';
+import {StoreComponent} from './pages/store/store.component';
+import {LibraryComponent} from './pages/library/library.component';
 
 const routes: Routes = [
   {
     path:'',
     component: MainComponent,
+    children: [
+      {
+        path:'',
+        component: StoreComponent
+      },
+      {
+        path: 'library',
+        component: LibraryComponent
+      }
+    ]
 
   }
 ];
